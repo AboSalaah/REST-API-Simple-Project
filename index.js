417dc18a5b161a258fbe8f8508c4657801817531
Ahmed Saleh <abosalaah96@gmail.com>
2018-07-05 12:07:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+//setup express app
+const app = express();
+//listen to a specific port number for handling requests
+app.listen(3000,function(){
+   console.log("i'm listening noooooooow!");
+});
