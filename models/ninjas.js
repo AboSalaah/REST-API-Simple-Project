26488cc9c573ee5df6e9cac9b14b52adcbd99b84
Ahmed Saleh <abosalaah96@gmail.com>
2018-07-05 15:33:49 +0200

    defining the model and the schema for the ninjas data

---
+++ models/ninjas.js
@@ -0,0 +1,21 @@
+
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ninjaSchema = new Schema({
+    name:{
+        type: String,
+        required:[true,'Name is required']
+    },
+    rank:{
+        type:String
+    },
+    available:{
+       type:Boolean,
+       default:false
+    }
+});
+
+const Ninja = mongoose.model('ninja',ninjaSchema);
+
+module.exports = Ninja;
